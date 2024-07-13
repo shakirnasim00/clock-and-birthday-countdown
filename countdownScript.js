2073d7a5be027c0af6cc76b344b1c2ee2f3f2e3a
shakirnasim00 <shak.nasim@gmail.com>
2024-07-12 17:31:42 -0700

    fixed whitespace

--- countdownScript.js
+++ countdownScript.js
@@ -6,8 +6,6 @@ const milliseconds30Days =  30.44 * 24 * 60 * 60 * 1000;
 
 const elements = document.getElementsByClassName("unit-container");
 
-
-
 // updates every second with the correct time information
 var update = setInterval(function() {
 
@@ -28,7 +26,7 @@ var update = setInterval(function() {
   document.getElementById("countdown-timer-minutes").innerHTML = minutes;
   document.getElementById("countdown-timer-seconds").innerHTML = seconds;
 
-  for (let element of elements) { //For loop to run through each element of the class("unit-container" and edit multiple elements
+  for (let element of elements) { //For loop to run through each element of the class("unit-container") and edit multiple elements
     if (differenceOfDate < milliseconds30Days) {
         element.style.color = "red";
     } else if (differenceOfDate < (milliseconds30Days * 6) && differenceOfDate >= milliseconds30Days) {
